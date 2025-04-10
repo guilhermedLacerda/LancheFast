8cbb09dd2bd9efba6bb22a63ed0f1c447febbf45
guilhermedlacerda <guilhermedossantoslacerda@gmail.com>
2025-04-10 13:04:32 -0300

    Implementado, Model, Migrate, Index e seed do crud de Produtos

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,21 @@
 <?php
 
-use App\Livewire\Clientes\Create;
-use App\Livewire\Clientes\Edit;
-use App\Livewire\Clientes\Index;
-use App\Livewire\Clientes\Show;
+use App\Livewire\Produto\ProdutoCreate;
+use App\Livewire\Produto\ProdutoEdit;
+use App\Livewire\Produto\ProdutoShow;
+use App\Livewire\Produto\ProdutoIndex;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('clientes')->group(function() {
+Route::prefix('clientes')->group(function () {
     Route::get('/', \App\Livewire\Clientes\Index::class)->name('clientes.index');
     Route::get('/create', \App\Livewire\Clientes\Create::class)->name('clientes.create');
-    Route::get('/{clientes}', \App\Livewire\Clientes\Show::class)->name('clientes.show');
-    Route::get('/{clientes}/edit', \App\Livewire\Clientes\Edit::class)->name('clientes.edit');
+    Route::get('/{cliente}', \App\Livewire\Clientes\Show::class)->name('clientes.show');
+    Route::get('/{cliente}/edit', \App\Livewire\Clientes\Edit::class)->name('clientes.edit');
+});
+
+Route::prefix('produtos')->group(function () {
+    Route::get('/', ProdutoIndex::class)->name('produtos.index');
+    Route::get('/create', ProdutoCreate::class)->name('produtos.create');
+    Route::get('/{produto}', ProdutoShow::class)->name('produtos.show');
+    Route::get('/{produto}/edit', ProdutoEdit::class)->name('produtos.edit');
 });
